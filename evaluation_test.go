826e08c0c7a1d2d50ae327fbd6a7cdeeca8cfafa
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-29 16:25:08 +0200

    add failing test for evaluation and remove randomization for deposit withdraw

--- evaluation_test.go
+++ evaluation_test.go
@@ -7,9 +7,21 @@ func TestEmptySolutionEvaluation(t *testing.T) {
 	solution := Solution{}
 	score, err := scenario.evaluate(solution)
 	if err != nil {
-		t.Errorf("evaluating empty solution should not return an error #{err}")
+		t.Errorf("evaluating empty solution should not return an error %v", err)
 	}
 	if score != 0 {
-		t.Errorf("score should be 0 and not #{score}")
+		t.Errorf("score should be 0 and not %d", score)
+	}
+}
+
+func TestSolutionForLargeScenarioWithDepositEvaluation(t *testing.T) {
+	scenario := largeScenarioWithDeposit()
+	solution := solutionForLargeScenarioWithDeposit()
+	score, err := scenario.evaluate(solution)
+	if err != nil {
+		t.Errorf("evaluating empty solution should not return an error %v", err)
+	}
+	if score != 24 {
+		t.Errorf("score should be 24 and not %d", score)
 	}
 }
--- fixtures.go
+++ fixtures.go
@@ -35,6 +35,49 @@ func scenarioWithDeposit() Scenario {
 		height:    15,
 		deposits:  []Deposit{{position: Position{5, 5}, width: 5, height: 5, subtype: 0}},
 		obstacles: []Obstacle{},
-		turns:     100,
+		products: []Product{{
+			subtype:   0,
+			points:    10,
+			resources: []int{1, 0, 0, 0, 0, 0, 0, 0},
+		}},
+		turns: 10,
+	}
+}
+
+func largeScenarioWithDeposit() Scenario {
+	return Scenario{
+		width:     20,
+		height:    20,
+		deposits:  []Deposit{{position: Position{0, 0}, width: 5, height: 5, subtype: 0}},
+		obstacles: []Obstacle{},
+		products: []Product{{
+			subtype:   0,
+			points:    10,
+			resources: []int{1, 0, 0, 0, 0, 0, 0, 0},
+		}},
+		turns: 10,
+	}
+}
+
+func solutionForLargeScenarioWithDeposit() Solution {
+	return Solution{
+		factories: []Factory{{
+			position: Position{
+				x: 9,
+				y: 0,
+			},
+			product:         0,
+			resourceStorage: nil,
+		}},
+		mines: []Mine{{
+			position: Position{
+				x: 6,
+				y: 1,
+			},
+			direction:        0,
+			resourcesIngress: nil,
+			resourcesEgress:  nil,
+		}},
+		conveyors: nil,
 	}
 }
