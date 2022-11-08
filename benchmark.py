5db2b858a259aeb5774083fa5aca0fc36966f668
Richard Wohlbold <richard@rgwohlbold.de>
2022-11-08 16:41:48 +0100

    benchmarking pipeline (#11)
    * Add benchmarking code
    
    * Install and setup lpsolve
    
    * Try to install package with sudo
    
    * Use custom benchmarking tool
    
    * Add github token
    
    * Add empty line to build.yaml
    
    * Average statistics over three runs
    
    * Run only on master
    
    * Always execute
    
    * Revert "Always execute"
    
    This reverts commit 89e53dd114cdb98297377c99efe437ef45390192.
    
    * Fix benchmark.py
    
    * Revert "Revert "Always execute""
    
    This reverts commit 17e98ec2ae3b7d71e940bd42b17a14499c0c123e.
    
    * Fix float issues

---
+++ benchmark.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import json
+import os
+import subprocess
+import sys
+import time
+
+NUM_RUNS_PER_FILE = 3
+
+
+def output_to_fitness(output):
+    for line in output.decode().split("\n"):
+        if "final fitness" in line:
+            fitness = int(line.split(" ")[-1])
+            return fitness
+
+
+def input_to_benchmark_dicts(file):
+    output_file = file + ".out"
+    start = time.time()
+    p = subprocess.Popen(["./profit-solver-icup23", "-input", "tasks/" + file, "-output", output_file], stderr=subprocess.PIPE)
+    p.wait()
+    end = time.time()
+
+    if p.returncode != 0:
+        print("return code", p.returncode)
+        print("stderr", p.stderr.read())
+        exit(1)
+    os.unlink(output_file)
+    return output_to_fitness(p.stderr.read()), end-start
+
+
+if __name__ == '__main__':
+    l = []
+    for file in os.listdir("tasks"):
+        print(file, file=sys.stderr)
+        total_time = 0
+        total_fitness = 0
+        for _ in range(NUM_RUNS_PER_FILE):
+            fitness, elapsed_time = input_to_benchmark_dicts(file)
+            total_time += elapsed_time
+            total_fitness += fitness
+        fitness_dict = {
+            "name": f"{file} - fitness",
+            "unit": "points",
+            "value": total_fitness / NUM_RUNS_PER_FILE,
+        }
+        time_dict = {
+            "name": f"{file} - time",
+            "unit": "seconds",
+            "value": total_time / NUM_RUNS_PER_FILE,
+        }
+        l.append(fitness_dict)
+        l.append(time_dict)
+    print(json.dumps(l))
+
