89859c9c2e8d2942493955196fe2a6294be5216a
Richard Wohlbold <richard@rgwohlbold.de>
2022-12-28 16:51:57 +0100

    Revert changes in priorityQueue.go

--- priorityQueue.go
+++ priorityQueue.go
@@ -17,7 +17,7 @@ func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
 	// We want Pop to give us the lowest, not highest, priority so we use smaller than here.
-	return pq[i].priority < pq[j].priority
+	return pq[i].priority > pq[j].priority
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
