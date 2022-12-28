c3bbb25c7238f38d02b410b40f932e38f8628e39
Richard Wohlbold <richard@rgwohlbold.de>
2022-12-28 16:36:59 +0100

    Add test for TheoreticalOptimum()

---
+++ optimum_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestOptimumTask1(t *testing.T) {
+	scenario, _, err := importFromProfitJson("official_tasks/002.task.json")
+	if err != nil {
+		t.Errorf("failed to import fixture: %e", err)
+	}
+	optimum, err := TheoreticalOptimum(scenario)
+	if optimum != 120 {
+		t.Errorf("optimum should be 120")
+	}
+}
