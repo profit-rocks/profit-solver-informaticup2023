e29e3197a1f2a4b5aeeaa1220c17f547c851f8b7
Alexander Sohn <github@asohn.de>
2023-01-01 11:56:50 +0100

    Use priorityQueue and heap the right way.

--- conveyor.go
+++ conveyor.go
@@ -351,7 +351,7 @@ func (g *GeneticAlgorithm) path(chromosome Chromosome, startPosition Position, e
 	var factory *Factory
 	var initialDistance int
 	for queue.Len() > 0 {
-		current := queue.Pop().(*Item)
+		current := heap.Pop(&queue).(*Item)
 		currentConveyor := current.value
 		currentEgress := current.value.Egress()
 		finished := false
@@ -408,7 +408,7 @@ func (g *GeneticAlgorithm) path(chromosome Chromosome, startPosition Position, e
 						value:    nextConveyor,
 						priority: current.priority + 1,
 					}
-					queue.Push(&next)
+					heap.Push(&queue, &next)
 					cellInfo[nextEgress.y][nextEgress.x].previousEgress = current.value.Egress()
 					cellInfo[nextEgress.y][nextEgress.x].currentConveyor = nextConveyor
 					cellInfo[nextEgress.y][nextEgress.x].distance = next.priority
